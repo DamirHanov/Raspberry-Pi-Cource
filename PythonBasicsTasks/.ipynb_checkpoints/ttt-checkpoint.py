71f503c3bad39e2db7eea38b6c7a7d9087a72b65
Damir Hanov <damir.khanof@yandex.ru>
2020-12-15 22:14:15 +0500

    Removed ipynb checkpoints

--- PythonBasicsTasks/.ipynb_checkpoints/ttt-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import wave
-import random
-
-with wave.open('test.wav', 'wb') as file:
-    file.setnchannels(1)
-    file.setsampwidth(1)
-    file.setframerate(256)
-    for x in range(256 * 10):
-        file.writeframes(b'10101110')
